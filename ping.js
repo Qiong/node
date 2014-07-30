bdfc2e573400192457dc873df2f51a66a2bd51d2
Gary <ynchen@685b35bfa4ce.ant.amazon.com>
2014-07-30 16:38:43 -0700

    1

--- ping.js
+++ ping.js
@@ -1,33 +1,7 @@
 var Monitor = require('ping-monitor');
 
-
 var sitex-monitor = new Monitor({
-    website: 'http://google.com,
+    website: 'http://google.com',
     timeout: 10
 });
 
-sitex-monitor.on('error', function (msg) {
-    console.log(msg);
-});
-
-
-sitex-monitor.on('up', function (res) {
-    console.log('Yay!! ' + res.website + ' is up.');
-});
-
-
-sitex-monitor.on('down', function (res) {
-    console.log('Shit!! ' + res.website + ' is down! ' + res.statusMessage);
-});
-
-
-// this event is required to be handled in all Node-Monitor instances
-sitex-monitor.on('error', function (res) {
-    console.log('Oh Shit!! An unexpected error occured trying to load ' + res.website + '!');
-    sitex-monitor.stop();
-});
-
-
-sitex-monitor.on('stop', function (website) {
-    console.log(website + ' monitor has stopped.');
-});
