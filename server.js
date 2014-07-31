9cb43ca277cf4cf0aa52cb1d6c779f06bf6082a3
cyberyc <qiongfei.seas@gmail.com>
2014-07-30 22:51:10 -0700

    svr

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 
 
 var http = require ("http")
-
+var url = require("url");
 http.createServer(function(request, response) {
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello World");
+	//response.writeHead(200, {"Content-Type": "text/plain"});
+	response.writeHead(200, {"Content-Type": "text/html"});
+	response.write("<!DOCTYPE html>");
+	response.write("<html>");
+	response.write("<head>");
+	response.write("<form method=get>")
+	response.write("IP Address: <input type='text' name='IP' value=''>")
+	response.write("<br><br><br>")
+	response.write("<input type='submit' value='PING TESTING'>");
+	response.write("</form>")
+	response.write("</head>");
+	response.write("<body>");
+	response.write("</body>");
+	response.write("</html>");
+	//response.write("Hello World");
+	var params = url.parse(request.url,true).query;
+	//console.log(params);
+	var a = params.number1;
+	var numA = new Number(a);
+	response.write(numA);
 	response.end();
 }).listen(8888);
