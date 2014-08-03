70259bf1600564c47a08372c8ad2df18f7e64078
cyberyc <qiongfei.seas@gmail.com>
2014-08-02 22:36:10 -0700

    ping

--- server.js
+++ server.js
@@ -1,26 +1,48 @@
 
 
-var http = require ("http")
+var http = require ("http");
 var url = require("url");
+//var sys = require('sys');
+var ping = require('./lib/ping');
+var sys = require('util')
+
 http.createServer(function(request, response) {
 	//response.writeHead(200, {"Content-Type": "text/plain"});
 	response.writeHead(200, {"Content-Type": "text/html"});
 	response.write("<!DOCTYPE html>");
 	response.write("<html>");
-	response.write("<head>");
-	response.write("<form method=get>")
-	response.write("IP Address: <input type='text' name='IP' value=''>")
+	response.write("<body>");
+	response.write("<form method= 'GET' action='/PING'>")
+	response.write("IP Address: <input type='text' name='IP' >")
 	response.write("<br><br><br>")
-	response.write("<input type='submit' value='PING TESTING'>");
+	response.write("<input type='submit' value='PIN'>");
 	response.write("</form>")
-	response.write("</head>");
 	response.write("<body>");
 	response.write("</body>");
 	response.write("</html>");
-	if (req.method == "POST"){
-		console.log(req.body.IP)
+	console.log(request.url)
+	var requrl= request.url
+	//console.log(url.parse(request.url).pathname)
+	if (url.parse(request.url).pathname == "/PING"){
+		console.log(request.url)
+		console.log(url.parse(requrl).query)
+		var IP = getIP(url.parse(requrl).query)
+		console.log(IP)
+		var result = ping(IP)
+		response.write("IP received, Ping result is");
+		response.write(result)
 	}
-
 
 	response.end();
 }).listen(8888);
+
+function getIP(string){
+	var string = string
+	return string.substr(3)
+}
+
+function ping(IP){
+	var ip = IP
+	console.log('runs here')
+	
+}
