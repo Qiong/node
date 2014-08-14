418a197cdf277765941815a21370d4c0a7fdd05c
cyberyc <qiongfei.seas@gmail.com>
2014-08-13 21:59:58 -0700

    a

--- action/chatrooms/public/javascripts/chat_ui.js
+++ action/chatrooms/public/javascripts/chat_ui.js
@@ -5,3 +5,20 @@ function divEscapedContentElement(message) {
 function divSystemContentElement(message) {
   return $('<div></div>').html('<i>' + message + '</i>');
 }
+
+function processUserInput(chatApp,socket){
+	var message = $('#send-message').val();
+	var systemMessage;
+
+	if (message.chatAt(0)=='/'){
+		systemMessage = chatApp.processCommand(message);
+		if (systemMessage){
+			$('#message').append(divSystemContentElement(systemMessage));
+		}
+	} else{
+		chatApp.sendMessage($('#room').text(),message);
+		$('#message').append(divEscapedContentElement(message));
+		$('#message').scrollTop($('#message').prop('scrollHeight'));
+	}
+	$('#send-message').val('');
+}
